14c6dcfca3dad33b593938d7ecb2dda3c7e9e4ec
amr44-97 <amrabdo4790@gmail.com>
2022-05-14 13:14:10 +0200

    add dwm-flex st-flex

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,40 @@
 
 # sourcees 
-eval (opam env --switch=4.11.1)
-export PATH="$PATH:$HOME/.cargo/bin:$HOME/.cabal/bin/xmonad:/home/amr/.local/bin:/home/amr/.nimble/bin:"
-export DOTNET_ROOT=/snap/dotnet-sdk/current
-
-
-
+#eval (opam env --switch=4.11.1)
+export PATH="$PATH:$HOME/.cargo/bin:$HOME/.cabal/bin/xmonad:/home/amr/.local/bin:/home/amr/.nimble/bin:/home/amr/.zig-linux-x86_64-0.10.0:"
+export PATH="/home/amr/.nimble/bin:$PATH"
+export PATH="/home/amr/go/bin:$PATH"
+export PATH="/usr/local/go/bin:$PATH"
+export PATH="/home/amr/.zig-linux-x86_64-0.10.0:$PATH"
+export PATH="/opt/zig-linux-x86_64-0.10:$PATH"
+#eval (opam env)
+export PATH="$PATH:/home/amr/.ghcup/bin"
+export PATH="$PATH:$HOME/.cabal/bin"
 
 # aliases 
-alias vi        'nvim' 
-alias ls        'lsd'   
-alias ll        'lsd -al'   
-alias vim       'nvim'   
+alias vi        'nvim'
+alias فم        'nvim'
+
+alias vim      'neovide'
+alias v      'neovide'
+alias nv      'neovide'
+alias cls      'clear'
+alias clr      'clear'
+
+alias nivm      'nvim' 
+alias ls        'lsd'
+alias لس        'lsd'
+alias ll        'lsd -al'
+alias اسدل        'lsd -al'
+alias نظ        'clear'
+alias اذهب      ' cd'
 alias mpc       'mpv --no-video *'   
-alias vi        'nvim'   
 alias grep      'grep --colour=auto'   
 alias egrep     'grep -E --colour=auto'   
 alias fgrep     'grep -F --colour=auto'   
 
+
+
+function fish_greeting
+end
 
