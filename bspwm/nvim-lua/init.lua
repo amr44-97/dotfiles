5d05d7349a7ece75dc7a49d2f6b32ba4341bf577
amr44-97 <amrabdo4790@gmail.com>
2022-05-16 05:25:56 +0200

    config dmenu for bspwm

--- bspwm/nvim-lua/init.lua
+++
@@ -1,9 +0,0 @@
-require "options"
-require "colorscheme"
-require "keymaps"
-require "plugins"
-require "setup"
-require "nvim-cmp"
-require "lsp"
-
-
--- bspwm/nvim-lua/lua/lsp/settings/zls.lua
+++
@@ -1,9 +0,0 @@
---require'lspconfig'.zls.setup{}
-return {
-   languageserver = {
-       zls = {
-           cmd = {"/home/amr/.local/bin/zls"},
-           filetypes =  {"zig"},
-       }
-   }
-}
