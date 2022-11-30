251e6a549855c9156f9c7c6840343568aa5bcc28
amr44-97 <amrabdo4790@gmail.com>
2022-12-01 01:15:19 +0200

    dwm flexipatch

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,5 @@
+(setq package-native-compile t)
+
 (setq user-full-name "Amr Maharek")
 (setq inhibit-startup-message t)
 
@@ -36,7 +38,9 @@
 (global-display-line-numbers-mode 1)
 ;; Theme - Font
 (set-face-attribute 'default nil :font "CaskaydiaCove Nerd Font" :height 120)
-(load-theme 'modus-vivendi t)
+;(load-theme 'modus-vivendi t)
+;(load-theme 'doom-solarized-dark-high-contrast t)
+(load-theme 'doom-solarized-dark t)
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -49,7 +53,7 @@
 
 
 (setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs 
-  projectile hydra flycheck company avy which-key  dap-mode ))
+  projectile hydra flycheck company avy which-key  dap-mode  php-mode emmet-mode))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -62,10 +66,23 @@
 
 (use-package doom-themes)
 
-
 (use-package nasm-mode)
 (add-to-list 'auto-mode-alist '("\\.asm\\'" . nasm-mode))
 
+
+;; Web Mode Setup
+(use-package web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+;(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(setq web-mode-ac-sources-alist
+  '(("css" . (ac-source-css-property))
+    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
+(setq web-mode-enable-auto-pairing t)
+
+;;;;;;;
 
 (use-package vertico
   :init
@@ -108,28 +125,28 @@
 (use-package all-the-icons
   :ensure t)
 
-(use-package minions
-  :hook (doom-modeline-mode . minions-mode))
+;(use-package minions
+;  :hook (doom-modeline-mode . minions-mode))
 
 (use-package doom-modeline
   :ensure t
-  :hook (after-init  . doom-modeline-mode)
-  :custom-face
-  (mode-line ((t (:height 0.90))))
-  (mode-line-inactive ((t (:height 0.85))))
-  :custom
-  (doom-modeline-height 15)
-  (doom-modeline-bar-width 6)
-  (doom-modeline-lsp t)
-  (doom-modeline-github nil)
-  (doom-modeline-mu4e nil)
-  (doom-modeline-irc nil)
-  (doom-modeline-minor-modes t)
-  (doom-modeline-persp-name nil)
-  (doom-modeline-buffer-file-name-style 'truncate-except-project)
-  (doom-modeline-major-mode-icon t)
-  (setq doom-modeline-buffer-state-icon t)
-  (doom-modeline-icon t))
+  :hook (after-init  . doom-modeline-mode))
+;  :custom-face
+;  (mode-line ((t (:height 0.90))))
+;  (mode-line-inactive ((t (:height 0.85))))
+;  :custom
+; (doom-modeline-height 15)
+; (doom-modeline-bar-width 6)
+; (doom-modeline-lsp t)
+; (doom-modeline-github nil)
+; (doom-modeline-mu4e nil)
+; (doom-modeline-irc nil)
+; (doom-modeline-minor-modes t)
+; (doom-modeline-persp-name nil)
+; (doom-modeline-buffer-file-name-style 'truncate-except-project)
+; (doom-modeline-major-mode-icon t)
+; (setq doom-modeline-buffer-state-icon t)
+; (doom-modeline-icon t))
 
 
 
@@ -147,10 +164,16 @@
 
 (use-package zig-mode)
 
+
+;;;; LSP Hooks
 (which-key-mode)
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 (add-hook 'zig-mode-hook 'lsp)
+(add-hook 'php-mode-hook 'lsp)
+(add-hook 'web-mode-hook 'lsp)
+
+
 
 (setq lsp-zig-zls-executable "/opt/zig-linux-x86_64-dev/zls")
 (setq zig-zig-bin "/opt/zig-linux-x86_64-dev/zig")
@@ -165,6 +188,7 @@
 (with-eval-after-load 'lsp-mode
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
   (require 'dap-cpptools)
+  (require 'dap-php)
   (yas-global-mode))
 
 
@@ -286,7 +310,7 @@
 (define-key evil-motion-state-map (kbd "SPC .") 'find-file )
 (define-key evil-motion-state-map (kbd "SPC c c") 'compile )
 
-
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Daemon Settings
@@ -295,3 +319,19 @@
 	      (lambda (frame)
 		(setq doom-modeline-icon t)
 		(interactive) (load-file "~/.config/emacs/setup/emacs-server-frame.el"))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "0c08a5c3c2a72e3ca806a29302ef942335292a80c2934c1123e8c732bb2ddd77" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" default))
+ '(package-selected-packages
+   '(vterm-toggle all-the-icons-dired lsp-mode yasnippet lsp-treemacs projectile hydra flycheck company avy which-key dap-mode php-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "SAJA" :family "CaskaydiaCove Nerd Font")))))
--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,16 @@ export PATH="/opt/lua-language-server/bin:/opt/zls-x86_64:$PATH"
 export PATH="/opt/Lua-Language-Server/bin:$PATH"
 export PATH="/opt/zls/zig-out/bin:$PATH"
 export PATH="/opt/go/bin:$PATH"
-export PATH="$PATH:$(go env GOPATH)/bin"
+#export PATH="$PATH:$(go env GOPATH)/bin"
 
 
 export BUN_INSTALL="/home/amr/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 export PATH="/home/amr/.cargo/bin:$PATH"
-export EDITOR='nvim'
+export EDITOR='emacsclient -tty -a emacs'
 
+[ -n "$SSH_CONNECTION" ] && unset SSH_ASKPASS
+export GIT_ASKPASS=
 
 
 
@@ -21,6 +23,7 @@ export EDITOR='nvim'
 
 # aliases 
 alias vi        'nvim'
+alias ed        'emacsclient -tty -a emacs -nw'
 
 alias vim      'nvim'
 alias v        'nvim'
